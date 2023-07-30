006b4b2a20b25f50e9efaa07efcede9259f441be
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2023-07-30 16:19:07 -0300

    Finish product and checkout pages

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,7 @@
-import { GetStaticPaths, GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next";
+import axios from "axios";
 import Image from "next/image";
+import { useState } from "react";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
@@ -11,10 +13,31 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
+
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false);
+
+  async function handleBuyButton() {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+    } catch (err) {
+      setIsCreatingCheckoutSession(false);
+
+      alert('Falha ao redirecionar ao checkout!')
+    }
+  }
 
   return (
     <ProductContainer>
@@ -28,7 +51,7 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyButton}>
           Comprar agora
         </button>
       </ProductDetails>
@@ -43,12 +66,16 @@ export const getStaticPaths: GetStaticPaths = async () => {
         params: { id: 'prod_OMKefDaDB4bJ8a' }
       }
     ],
-    fallback: false,
+    fallback: 'blocking',
   }
 }
 
-export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
-  const productId = params.id;
+export const getStaticProps: GetStaticProps<any, { id: string }> = async (context) => {
+  if (!context.params) {
+    throw new Error('`params` is undefined. This is unexpected in getStaticProps with dynamic routing.');
+  }
+
+  const productId = context.params.id;
 
   const product = await stripe.products.retrieve(productId, {
     expand: ['default_price']
@@ -66,7 +93,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           style: 'currency',
           currency: 'BRL'
         }).format(price.unit_amount !== null ? price.unit_amount / 100 : 0),
-        description: product.description
+        description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1 // 1 hours
